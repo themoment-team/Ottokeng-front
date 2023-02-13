acf6a6f7e9fdfb407527f996abc66c00ff991269
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2023-02-13 14:23:59 +0900

    Add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <header></header>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<MainPage />}></Route> */}
+          {/* <Route path="/login" element={<LoginPage />}></Route> */}
+          {/* <Route path="/write" element={<WritePage />}></Route> */}
+          {/* <Route path="/list" element={<ListPage />}></Route> */}
+          {/* <Route path="/list:id" element={<Bulletin />}></Route> */}
+          {/* <Route path="*" element={<NotFound />}></Route> */}
+        </Routes>
+      </BrowserRouter>
+      <footer></footer>
     </div>
   );
 }
