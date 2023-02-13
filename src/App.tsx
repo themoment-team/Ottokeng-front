c11ab0fe8b2bbe143bef586c93ae8440dc508b29
이정우 <comand123@naver.com>
2023-02-13 20:11:05 +0900

    컴포넌트 경로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { GoogleLoginBtnBefore, GoogleLoginBtnAfter } from './components';
 
 function App() {
   return (
     <div className="App">
       <header></header>
+
       <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<MainPage />}></Route> */}
-          {/* <Route path="/login" element={<LoginPage />}></Route> */}
+          <Route path="/login" element={<GoogleLoginBtnBefore />}></Route>
+          <Route
+            path="/login/login/*"
+            element={<GoogleLoginBtnAfter />}
+          ></Route>
           {/* <Route path="/write" element={<WritePage />}></Route> */}
           {/* <Route path="/list" element={<ListPage />}></Route> */}
           {/* <Route path="/list/:id" element={<Bulletin />}></Route> */}
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+export { default as GoogleLoginBtnBefore } from './GoogleLoginBtnBefore';
+export { default as GoogleLoginBtnAfter } from './GoogleLoginBtnAfter';
