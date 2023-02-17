3021222df268d7633aac1c45e350c4a68b25f62d
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2023-02-17 23:41:07 +0900

    Update style

--- src/components/ListItem/style.ts
+++ src/components/ListItem/style.ts
@@ -1,81 +1,81 @@
 /** @jsxImportSource @emotion/react */
 import styled from '@emotion/styled';
 
-export const Container = styled.div({
-  width: '874px',
-  padding: '60px 52px 60px 74px',
-  backgroundColor: '#fff',
-  marginTop: '40px',
-});
+export const Container = styled.div`
+  width: 874px;
+  padding: 60px 52px 60px 74px;
+  background-color: #fff;
+  margin-top: 40px;
+`;
 
-export const ContentBox = styled.div({
-  width: '874px',
-  height: '124px',
-  display: 'flex',
-});
+export const ContentBox = styled.div`
+  width: 874px;
+  height: 124px;
+  display: flex;
+`;
 
-export const IMGBox = styled.div({
-  width: '124px',
-  height: '124px',
-  borderRadius: '8px',
-  backgroundColor: '#F1F1F5',
-});
+export const IMGBox = styled.div`
+  width: 124px;
+  height: 124px;
+  border-radius: 8px;
+  background-color: #f1f1f5;
+`;
 
-export const Content = styled.div({
-  width: '726px',
-  height: '124px',
-  marginLeft: '24px',
-});
+export const Content = styled.div`
+  width: 726px;
+  height: 124px;
+  margin-left: 24px;
+`;
 
-export const Title = styled.h1({
-  fontSize: '19px',
-  color: '#191919',
-  margin: '5px 0 8px 0',
-});
+export const Title = styled.h1`
+  font-size: 19px;
+  color: #191919;
+  margin: 5px 0 8px 0;
+`;
 
-export const Inform = styled.p({
-  fontSize: '16px',
-  width: '655px',
-  color: '#6D6D6D',
-  marginBottom: '22px',
-  height: '38px',
-});
+export const Inform = styled.p`
+  font-size: 16px;
+  width: 655px;
+  color: #6d6d6d;
+  margin-bottom: 22px;
+  height: 38px;
+`;
 
-export const BottomBox = styled.div({
-  width: '726px',
-  height: '22px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-});
-export const DateAndNameBox = styled.div({
-  height: '22px',
-  alignItems: 'center',
-  display: 'flex',
-  justifyContent: 'center',
-});
+export const BottomBox = styled.div`
+  width: 726px;
+  height: 22px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const DateAndNameBox = styled.div`
+  height: 22px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
 
-export const DateAndName = styled.p({
-  fontSize: '13px',
-  color: '#A1A1A1',
-});
+export const DateAndName = styled.p`
+  font-size: 13px;
+  color: #a1a1a1;
+`;
 
-export const MessageBox = styled.div({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  height: '15px',
-});
+export const MessageBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 15px;
+`;
 
-export const NumberOfChat = styled.p({
-  fontSize: '12px',
-  color: '#999999',
-  marginLeft: '2px',
-});
+export const NumberOfChat = styled.p`
+  font-size: 12px;
+  color: #999999;
+  margin-left: 2px;
+`;
 
-export const Line = styled.div({
-  width: '872px',
-  height: '1px',
-  backgroundColor: '#ECECEC',
-  margin: '24px 0 36px 0',
-});
+export const Line = styled.div`
+  width: 872px;
+  height: 1px;
+  background-color: #ececec;
+  margin: 24px 0 36px 0;
+`;
--- src/components/WriteBox/style.ts
+++ src/components/WriteBox/style.ts
@@ -1,89 +1,89 @@
 /** @jsxImportSource @emotion/react */
 import styled from '@emotion/styled';
 
-export const Container = styled.div({
-  width: '820px',
-  height: '638px',
-  backgroundColor: '#fff',
-  padding: '41px 64px',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  boxShadow: '0 4px 10px 0 rgba(0, 0, 0, 0.1)',
-  borderRadius: '8px',
-});
+export const Container = styled.div`
+  width: 820px;
+  height: 638px;
+  background-color: #fff;
+  padding: 41px 64px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+`;
 
-export const Title = styled.h1({
-  width: '200px',
-  fontSize: '28px',
-  margin: '0 0 24px 0',
-  // height: '33px',
-  fontWeight: '500',
-});
+export const Title = styled.h1`
+  width: 200px;
+  font-size: 28px;
+  margin: 0 0 24px 0;
+  // height: 33px;
+  font-weight: 500;
+`;
 
-export const Input = styled.input({
-  width: '804px',
-  border: '1px solid #858585',
-  borderRadius: '8px',
-  padding: ' 0 0 0 16px',
-  marginBottom: '24px',
-  fontSize: '18px',
-  outline: 'none',
-});
+export const Input = styled.input`
+  width: 804px;
+  border: 1px solid #858585;
+  border-radius: 8px;
+  padding: 0 0 0 16px;
+  margin-bottom: 24px;
+  font-size: 18px;
+  outline: none;
+`;
 
-export const Checkbox = styled.input({
-  width: '24px',
-  height: '24px',
-  border: '1.2px solid #999999',
-  borderRadius: '4.8px',
-  margin: '0',
-  cursor: 'pointer',
-});
+export const Checkbox = styled.input`
+  width: 24px;
+  height: 24px;
+  border: 1.2px solid #999999;
+  border-radius: 4.8px;
+  margin: 0;
+  cursor: pointer;
+`;
 
-export const CheckboxText = styled.p({
-  marginLeft: '9.6px',
-  fontSize: '16px',
-  fontWeight: '500',
-});
+export const CheckboxText = styled.p`
+  margin-left: 9.6px;
+  font-size: 16px;
+  font-weight: 500;
+`;
 
-export const RegistrationBox = styled.div({
-  width: '134px',
-  heigth: '36px',
-  border: '1px solid #6695F1',
-  borderRadius: '8px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-evenly',
-  padding: '6.5px 0',
-  cursor: 'pointer',
-});
+export const RegistrationBox = styled.div`
+  width: 134px;
+  height: 36px;
+  border: 1px solid #6695f1;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 6.5px 0;
+  cursor: pointer;
+`;
 
-export const SubmitBTN = styled.button({
-  width: '144px',
-  height: '44px',
-  backgroundColor: '#2E6EEB',
-  fontSize: '20px',
-  color: '#fff',
-  borderRadius: '8px',
-  border: '0',
-  cursor: 'pointer',
-});
+export const SubmitBTN = styled.button`
+  width: 144px;
+  height: 44px;
+  background-color: #2e6eeb;
+  font-size: 20px;
+  color: #fff;
+  border-radius: 8px;
+  border: 0;
+  cursor: pointer;
+`;
 
-export const RegistrationText = styled.p({
-  fontSize: '16px',
-  color: '#6695F1',
-  fontWeight: '500',
-});
+export const RegistrationText = styled.p`
+  font-size: 16px;
+  color: #6695f1;
+  font-weight: 500;
+`;
 
-export const Box = styled.div({
-  display: 'flex',
-  width: '820px',
-  marginRight: '0',
-  justifyContent: 'space-between',
-});
+export const Box = styled.div`
+  display: flex;
+  width: 820px;
+  margin-right: 0;
+  justify-content: space-between;
+`;
 
-export const FlexBox = styled.div({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-});
+export const FlexBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
