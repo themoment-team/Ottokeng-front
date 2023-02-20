7d03beac499a90434c23ed2baca643ebe006a942
이승제 <106712562+Lee-Seungje@users.noreply.github.com>
2023-02-20 16:47:46 +0900

    Add s3Upload function

--- src/components/WriteBox/index.tsx
+++ src/components/WriteBox/index.tsx
@@ -4,7 +4,8 @@ import * as I from '../../assets/svgs';
 import { css } from '@emotion/react';
 import { CheckBox } from './CheckBox';
 import { RegistrationBox } from './RegistrationBox';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import S3 from 'react-aws-s3-typescript';
 
 const WriteBox = ({
   kind,
@@ -17,6 +18,29 @@ const WriteBox = ({
   const [content, setContent] = useState<string>('');
   const [check, setCheck] = useState<string>('');
   const [picture, setPicture] = useState<any>();
+
+  // interface configInterFace {
+  //   bucketName: string;
+  //   region: string;
+  //   accessKeyId: string;
+  //   secretAccessKey: string;
+  // }
+
+  const config = {
+    bucketName: process.env.REACT_APP_BUCKET_NAME!,
+    region: process.env.REACT_APP_REGION!,
+    accessKeyId: process.env.REACT_APP_ACCESS!,
+    secretAccessKey: process.env.REACT_APP_SECRET!,
+  };
+
+  const s3Upload = async (picture: File) => {
+    const ReactS3Client = new S3(config);
+    ReactS3Client.uploadFile(picture, picture.name)
+      .then(data => {
+        console.log(data.location);
+      })
+      .catch(err => console.error(err));
+  };
 
   const changeChecked = (e: string) => {
     const delCheck = document.getElementById(`${e}CB`) as HTMLInputElement;
@@ -36,6 +60,7 @@ const WriteBox = ({
     if (title !== '' && content !== '' && check !== '') {
       //통과
       console.log('좋아');
+      s3Upload(picture);
     } else {
       // 거름
       console.log('안 돼 돌아가');
