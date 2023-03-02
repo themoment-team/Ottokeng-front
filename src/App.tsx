884583e8f63e0ef56575f822580bf004d3668b64
이정우 <comand123@naver.com>
2023-03-02 20:37:41 +0900

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Theme } from '@emotion/react';
-import {
-  GoogleLoginBtnBefore,
-  GoogleLoginBtnAfter,
-  Footer,
-  Header,
-  BoardInfo,
-} from './components';
+import { Header } from './components';
 
 function App() {
+  enum PATH {
+    login = '/login',
+    loginUser = 'login/login/*',
+    list = '/list/',
+  }
   return (
     <div className="App">
       <Header />
@@ -17,18 +15,18 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<MainPage />}></Route> */}
-          <Route path="/login" element={<GoogleLoginBtnBefore />}></Route>
-          <Route
-            path="/login/login/*"
+          {/* <Route path={PATH.login} element={<Login />}></Route> */}
+          {/* <Route
+            path={PATH.loginUser}
             element={<GoogleLoginBtnAfter />}
           ></Route>
-          {/* <Route path="/write" element={<WritePage />}></Route> */}
+          <Route path="/write" element={<WritePage />}></Route> */}
           {/* <Route path="/list" element={<ListPage />}></Route> */}
-          <Route path="/list/" element={<BoardInfo />}></Route>
+          {/* <Route path={PATH.list} element={<BoardInfo />}></Route> */}
           {/* <Route path="*" element={<NotFound />}></Route> */}
         </Routes>
       </BrowserRouter>
-      <Footer />
+      <footer />
     </div>
   );
 }
