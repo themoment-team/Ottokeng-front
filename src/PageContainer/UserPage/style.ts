2d8f25711d71534fb4627de4a2770331179fad9d
예빈 <103906632+yebin0310@users.noreply.github.com>
2023-03-18 17:47:59 +0900

    Update style

--- src/PageContainer/UserPage/style.ts
+++ src/PageContainer/UserPage/style.ts
@@ -4,9 +4,9 @@ import styled from '@emotion/styled';
 export const Container = styled.div({
   position: 'absolute',
   width: '100%',
-  height: '800px',
-  left: '0px',
-  top: '120px',
+  height: '50rem',
+  left: '0rem',
+  top: '7.5rem',
   background: '#DDE8F8',
   display: 'flex',
   justifyContent: 'center',
