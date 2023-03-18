5a866ff1ccdd4268a6fadf11fd6aea216a5f00a0
Lee-Seungje <moondgod@gmail.com>
2023-03-19 00:00:15 +0900

    Add get Location function

--- src/PageContainer/WritePage/index.tsx
+++ src/PageContainer/WritePage/index.tsx
@@ -13,7 +13,7 @@ const WriteBox = () => {
   const [content, setContent] = useState<string>('');
   const [check, setCheck] = useState<string>('');
   const [imgList, setImgList] = useState<any>([]);
-  const [location, setLocation] = useState<string>('address');
+  const [location, setMap] = useState<string[]>([]);
 
   let isUpdate = false;
 
@@ -72,16 +72,17 @@ const WriteBox = () => {
   };
 
   const handleSubmit = () => {
+    console.log(location);
     if (
       title !== '' &&
       content !== '' &&
       check !== '' &&
       (imgList[0] ?? false) &&
-      location !== ''
+      location.length !== 0
     ) {
       //통과
       const url =
-        'http://10.120.74.187:8080/post/writing' + isUpdate && state.id;
+        'https://server.ottokeng.site/post/writing' + (isUpdate && state.id);
       sendData(url);
     } else {
       // 거름
@@ -90,11 +91,14 @@ const WriteBox = () => {
   };
 
   const delServerIMG = async (picture: string) => {
-    const res = await axios.delete(`/post/writing/image/${picture}`, {
-      headers: {
-        Authorization: '',
+    const res = await axios.delete(
+      `https://server.ottokeng.site/post/writing/image/${picture}`,
+      {
+        headers: {
+          Authorization: '',
+        },
       },
-    });
+    );
   };
 
   interface updateProps {
@@ -147,21 +151,10 @@ const WriteBox = () => {
       setContent(state.contents ?? '');
       setCheck(state.type ?? '');
       setImgList(state.imageUrls ?? []);
-      setLocation(state.address ?? '');
+      setMap(state.address ?? '');
       changeChecked(state.type === 'LOST_WRITING' ? 'loss' : 'acquire');
     }
   }, []);
-
-  interface UserProps {
-    name: string;
-    userImg: string;
-  }
-
-  let init: UserProps = {
-    name: 'Anon',
-    userImg: 'img',
-  };
-  const [header, setHeader] = useState<UserProps>(init);
 
   return (
     <>
@@ -171,7 +164,7 @@ const WriteBox = () => {
           <C.IMGModal imgList={imgList} onRemove={onRemove} />
         </dialog>
         <dialog className="MapModal" css={ModalDesign}>
-          <C.MapModal />
+          <C.MapModal setMap={setMap} />
         </dialog>
         <input
           type="file"
--- src/components/MapModal/index.tsx
+++ src/components/MapModal/index.tsx
@@ -1,16 +1,71 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import * as S from './style';
+import { Map } from 'react-kakao-maps-sdk';
+import { useEffect } from 'react';
 
-const MapModal = () => {
+const MapModal = ({ setMap }: { setMap: Function }) => {
+  interface props {
+    locPosition: any;
+  }
+
+  let lat: number = 35.142738601752846,
+    lon: number = 126.80072297715732;
+  useEffect(() => {
+    const mapContainer = document.getElementById('mapLocation'),
+      mapOption = {
+        center: new kakao.maps.LatLng(lat, lon),
+        level: 3,
+      };
+
+    const map = new kakao.maps.Map(mapContainer as HTMLElement, mapOption);
+    const marker = new kakao.maps.Marker({
+      position: map.getCenter(),
+    });
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+
+        const locPosition = new kakao.maps.LatLng(lat, lon);
+        marker.setPosition(locPosition);
+
+        map.setCenter(locPosition);
+      });
+    } else {
+      alert('위치 접근 불가');
+    }
+
+    marker.setMap(map);
+
+    kakao.maps.event.addListener(
+      map,
+      'click',
+      function (mouseEvent: { latLng: any }) {
+        const latlng = mouseEvent.latLng;
+
+        marker.setPosition(latlng);
+        lat = latlng.getLat();
+        lon = latlng.getLng();
+        console.log(lat, lon);
+      },
+    );
+  }, []);
+
+  const handleSubmit = () => {
+    setMap([String(lat), String(lon)]);
+  };
+
   return (
     <S.MapModal>
-      <S.Map></S.Map>
+      <S.Map id="mapLocation" />
       <form method="dialog">
         <S.SubmitBTN
           css={css`
             margin-top: 9rem;
           `}
+          onClick={handleSubmit}
         >
           위치등록
         </S.SubmitBTN>
