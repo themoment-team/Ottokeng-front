3005f7c4320d7c959fcb7c4b3fb497c5f8fbf785
예빈 <yebin060310@gmail.com>
2023-03-18 17:12:40 +0900

    Update style

--- src/PageContainer/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as MainPage } from './MainPage';
-export { default as StartPage } from './StartPage';
-export { default as AboutPage } from './AboutPage';
-export { default as UserPage } from './UserPage';
