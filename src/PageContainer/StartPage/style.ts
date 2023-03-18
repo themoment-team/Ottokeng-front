e87998596d41acd8ffead350e1ed92ed81b3b3cc
예빈 <103906632+yebin0310@users.noreply.github.com>
2023-03-18 17:45:20 +0900

    Update style

--- src/PageContainer/StartPage/style.ts
+++ src/PageContainer/StartPage/style.ts
@@ -4,9 +4,9 @@ import styled from '@emotion/styled';
 export const Container = styled.div({
   position: 'absolute',
   width: '100%',
-  height: '800px',
-  left: '0px',
-  top: '120px',
+  height: '50rem',
+  left: '0rem',
+  top: '7.5rem',
   background: '#DDE8F8',
   display: 'flex',
   justifyContent: 'center',
@@ -15,17 +15,17 @@ export const Container = styled.div({
 
 export const Description = styled.div({
   position: 'absolute',
-  width: '285px',
-  height: '57px',
-  left: '260px',
-  top: '332px',
+  width: '17.8125rem',
+  height: '3.5625rem',
+  left: '16.25rem',
+  top: '20.75rem',
   color: '#383838',
 });
 
 export const Title = styled.div({
   position: 'absolute',
-  width: '577px',
-  height: '86px',
-  left: '260px',
-  top: '224px',
+  width: '36.0625rem',
+  height: '5.375rem',
+  left: '16.25rem',
+  top: '14rem',
 });
