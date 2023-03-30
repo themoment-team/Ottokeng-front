6daa939904642ddde2b87f72cd7fe8c63a2db128
이정우 <comand123@naver.com>
2023-03-30 20:45:00 +0900

    쓸때없는 코드 삭제

--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -1,8 +1,5 @@
-import { Link, useNavigate } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({ component }: any) => {
-  const navigate = useNavigate();
-
   return localStorage.getItem('token') ? (
     component
   ) : (
